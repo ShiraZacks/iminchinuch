67626a4f75859925023e2ed81d1b999482980281
Shira Zacks <shiramzacks@gmail.com>
2022-07-28 13:53:17 -0400

    if statement for months written

--- hebcal.js
+++ hebcal.js
@@ -1,6 +1,24 @@
 //getElementByClassName can't change css
 //use jQuery
 
+/*
+[
+  "Tishri",
+  "Heshvan",
+  "Kislev",
+  "Tevet",
+  "Shevat",
+  "Adar I",
+  "Adar",
+  "Nisan",
+  "Iyar",
+  "Sivan",
+  "Tamuz",
+  "Av",
+  "Elul",
+  "Adar II"]
+*/
+
 function month() {
 
   var weekday = new Intl.DateTimeFormat('en-u-ca-hebrew', { weekday: 'long' }).format(new Date());
@@ -10,11 +28,46 @@ function month() {
 
   console.log("Today is " + weekday + ", the "+ day + " of " + month +", " + year)
 
-  if (month == "Tamuz") {
-    $('.Tamuz').css('display', 'block')
+  if (month == "Tishri") {
+    $('.Tishrei').css('display', 'block')
         console.log("works");
-  }if (month == "Iyar"){
+  }else if (month == "Heshvan"){
+    $('.Cheshvan').css('display', 'block')
+    console.log("works");
+  }else if (month == "Kislev"){
+    $('.Kislev').css('display', 'block')
+    console.log("works");
+  }else if (month == "Tevet"){
+    $('.Teves').css('display', 'block')
+    console.log("works");
+  }else if (month == "Shevat"){
+    $('.Shevat').css('display', 'block')
+    console.log("works");
+  }else if (month == "Adar I"){
+    $('.AdarAleph').css('display', 'block')
+    console.log("works");
+  }else if (month == "Adar"){
+    $('.Adar').css('display', 'block')
+    console.log("works");
+  }else if (month == "Nisan"){
+    $('.Nissan').css('display', 'block')
+    console.log("works");
+  }else if (month == "Iyar"){
     $('.Iyar').css('display', 'block')
     console.log("works");
+  }else if (month == "Sivan"){
+    $('.Sivan').css('display', 'block')
+    console.log("works");
+  }else if (month == "Tamuz"){
+    $('.Tamuz').css('display', 'block')
+    console.log("works");
+  }else if (month == "Av"){
+    $('.Av').css('display', 'block')
+    console.log("works");
+  }else if (month == "Elul"){
+    $('.Av').css('display', 'block')
+    console.log("works");
+  }else{
+    console.log("error - js if statement broke")
   }
 }
