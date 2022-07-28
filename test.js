4690f6ccbad617d374eea3b554786ca375480dd7
Shira Zacks <shiramzacks@gmail.com>
2022-07-28 12:07:25 -0400

    testing

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
-function work(){
-  var month1 = new Intl.DateTimeFormat('en-u-ca-hebrew', { month: 'long' }).format(new Date(02/02/01));
+function monthname(){
+  const date = new Date(Date.UTC(6/6/2001))
+  var month1 = new Intl.DateTimeFormat('en-u-ca-hebrew', { month: 'long' }).format(date);
 
-
+  console.log(month1)
 }
-work()
+monthname()
 
 
