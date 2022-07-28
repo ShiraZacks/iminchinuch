ba4e989235329db6915d45879072aa4be0e0e841
Shira Zacks <shiramzacks@gmail.com>
2022-07-28 13:53:28 -0400

    tests

--- test.js
+++ test.js
@@ -1,8 +1,25 @@
 function monthname(){
-  const date = new Date(Date.UTC(6/6/2001))
-  var month1 = new Intl.DateTimeFormat('en-u-ca-hebrew', { month: 'long' }).format(date);
+  var month1 = new Intl.DateTimeFormat('en-u-ca-hebrew', { month: 'long' }).format(new Date());
 
   console.log(month1)
+/*
+[
+  "Tishri",
+  "Heshvan",
+  "Kislev",
+  "Tevet",
+  "Shevat",
+  "Adar I",
+  "Adar",
+  "Nisan",
+  "Iyar",
+  "Sivan",
+  "Tamuz",
+  "Av",
+  "Elul",
+  "Adar II"]
+*/
+
 }
 monthname()
 
