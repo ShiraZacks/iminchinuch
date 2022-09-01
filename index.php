6ce7e9ded990c61cd7bdf3d25775adc8fb2dd752
Shira Zacks <shiramzacks@gmail.com>
2022-09-01 16:38:08 -0400

    zmanim

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
     <link rel="shortcut icon" type="image/jpeg" href="favcon.jpg" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" charset="utf-8" src="hebcal.js?<?=time()?>"></script>
-    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?=time()?>"></script>
-    <link rel="stylesheet" href="style.css?<?=time()?>">
+    <script type="text/javascript" charset="utf-8" src="hebcal.js?<?= time() ?>"></script>
+    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
+    <link rel="stylesheet" href="style.css?<?= time() ?>">
     <?php include 'menu.php'; ?>
 </head>
 
@@ -26,9 +26,9 @@
     <!-- First section of text -->
     <div class="firstSmall">
         <!-- today's date -->
-<!--Start MyZmanim Widget for: https://iminchinuch.herokuapp.com/  -->
-    <script type="text/javascript" charset="UTF-8" language="javascript" src="https://www.myzmanim.com/widget.aspx?lang=en&mode=CandlesOnly&fsize=9&fcolor=f44336&hcolor=ff9a9a&bcolor=f44336&suf=s&key=oemDHmhHjMIzKzgvjacxn6n7DtZbV9WcOknBZ%2b%2fQrNt9%2bl7VvsBY9MxMagxAVoHxaBmjN9Nn3FL90m6QUto5NdGhivm1Y4RaIsfy3%2fvj%2fnqVGj2j84kXN17AQ%2bVngL93"></script><noscript style="font-family:verdana;">Find your daily zmanim at <a href="http://www.myzmanim.com/">MyZmanim.com</a>.</noscript>
-<!--End MyZmanim Widget-->
+        <!--Start MyZmanim Widget for: https://iminchinuch.herokuapp.com/  -->
+        <script type="text/javascript" charset="UTF-8" language="javascript" src="https://www.myzmanim.com/widget.aspx?lang=en&mode=CandlesOnly&fsize=12&fcolor=f44336&hcolor=757575&bcolor=f44336&suf=s&key=oemDHmhHjMIzKzgvjacxn6n7DtZbV9WcOknBZ%2b%2fQrNt9%2bl7VvsBY9MxMagxAVoHxaBmjN9Nn3FL90m6QUto5NdGhivm1Y4RaIsfy3%2fvj%2fnqVGj2j84kXN17AQ%2bVngL93"></script>
+        <!--End MyZmanim Widget-->
 
 
         <h2 id="hebrewDate"></h2>
@@ -53,7 +53,7 @@
                     <h1>Recent Updates</h1>
                     <h5 class="secondBold">Looked at a calendar recently? </h5>
                     <div id="hebcal" onload="shira()"></div>
-                    
+
 
                     <p class="secondSmall">This week's Parsha is .</p>
                     <p class="secondSmall">This month is .</p>
@@ -68,8 +68,8 @@
 
         <!-- bottom footer -->
         <footer>
-        <p class="footer">Website created by Shira Zacks. Please <a href="contact.php">contact me</a> if there are any mistakes; only Hashem
-            is perfect!</p>
+            <p class="footer">Website created by Shira Zacks. Please <a href="contact.php">contact me</a> if there are any mistakes; only Hashem
+                is perfect!</p>
         </footer>
 </body>
 
