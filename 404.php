cc6bc7fed9bd6e1eba1ed245327da8676d78bfa5
ShiraZacks <shiramzacks@gmail.com>
2022-12-14 17:29:05 -0500

    testing things

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eye Travel: Eye Care For The Home Bound</title>
+    <link rel="shortcut icon" type="image/png" href="eye.png" />
+    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
+    <link rel="stylesheet" href="style.css?<?= time() ?>">
+</head>
+<?php include "menu.php"; ?>
+
+<body>
+    <div class="div404 center">
+    <br><br><br>
+    <h1>Something went wrong!</h1>
+    <h2>This is a 404 error, which means that the link you clicked is broken or the page you are trying to access does not exist.</h2>
+    <h3 ><a class="link404" href="index.php">Click here to return to the Eye Travel homepage.</a></h3>
+    <br><br>
+</div><br>
+<?php include "footer.php"?>
+
+</body>
+</html>
