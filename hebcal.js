3f70970ba2c64837e11bc16afc06d17966bf881a
ShiraZacks <shiramzacks@gmail.com>
2022-12-15 11:56:53 -0500

    changing the roott variable --hide

--- hebcal.js
+++ hebcal.js
@@ -129,3 +129,14 @@ function monthName() {
 
 }
 monthName()
+
+
+
+var root = document.querySelector(':root');
+
+function rootChange_set() {
+  // Set the value of variable --hide to block
+  root.style.setProperty('--hide', 'block');
+};
+
+rootChange_set();
