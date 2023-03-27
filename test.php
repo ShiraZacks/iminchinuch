f91d037fbec8301b808bd99e5b9272115c7ec14e
ShiraZacks <shiramzacks@gmail.com>
2023-03-26 20:37:29 -0400

    added global vars!!!

--- test.php
+++ test.php
@@ -1,40 +1,41 @@
 <?php
+// Get current Gregorian date
+$gregorianDate = new DateTime();
+
+// Convert Gregorian date to Julian day count
+$julianDay = gregoriantojd($gregorianDate->format('m'), $gregorianDate->format('d'), $gregorianDate->format('Y'));
+
+// Calculate Jewish date from Julian day count
+$jDate = jdtojewish($julianDay);
+//echo $jDate,"   ";
+
+//change that str to  date
+$toTime = strtotime($jDate);
+$jewishDate = getdate($toTime);
+
+// Convert month number to its string representation
+$monthNames = [
+    1 => 'Tishrei',
+    2 => 'Cheshvan',
+    3 => 'Kislev',
+    4 => 'Teves',
+    5 => 'Shevat',
+    6 => 'Adar Aleph',
+    7 => 'Adar',
+    8 => 'Nisan',
+    9 => 'Iyar',
+    10 => 'Sivan',
+    11 => 'Tammuz',
+    12 => 'Av',
+    13 => 'Elul'
+];
+$monthStr = $monthNames[$jewishDate['mon']];
+// Store the final answer in a variable called "hebrewDate"
+$hebrewDate = $monthStr . ' ' . $jewishDate['mday'] . ', ' . $jewishDate['year'];
+
 function getHebrewDate()
 {
-    // Get current Gregorian date
-    $gregorianDate = new DateTime();
-
-    // Convert Gregorian date to Julian day count
-    $julianDay = gregoriantojd($gregorianDate->format('m'), $gregorianDate->format('d'), $gregorianDate->format('Y'));
-
-    // Calculate Jewish date from Julian day count
-    $jDate = jdtojewish($julianDay);
-    //echo $jDate,"   ";
-
-    //change that str to  date
-    $toTime = strtotime($jDate);
-    $jewishDate = getdate($toTime);
-
-    // Convert month number to its string representation
-    $monthNames = [
-        1 => 'Tishrei',
-        2 => 'Cheshvan',
-        3 => 'Kislev',
-        4 => 'Teves',
-        5 => 'Shevat',
-        6 => 'Adar Aleph',
-        7 => 'Adar',
-        8 => 'Nisan',
-        9 => 'Iyar',
-        10 => 'Sivan',
-        11 => 'Tammuz',
-        12 => 'Av',
-        13 => 'Elul'
-    ];
-    $monthStr = $monthNames[$jewishDate['mon']];
-    // Store the final answer in a variable called "hebrewDate"
-    $hebrewDate = $monthStr . ' ' . $jewishDate['mday'] . ', ' . $jewishDate['year'];
-
-echo "Today is $hebrewDate!"; // Outputs something like "Nisan 2, 5782"
-
+    global $hebrewDate; //global gets variable from outside the function
+echo "Today is $hebrewDate!"; // Outputs todays date in "Nisan 2, 5783" format
 }
+getHebrewDate();
