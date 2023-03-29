9f729f5e6cce3cb0518945d3af49e358c9152ba4
ShiraZacks <shiramzacks@gmail.com>
2023-03-29 17:48:51 -0400

    gave up the css, changing to database

---
+++ dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+$servername = 'localhost';
+$username = 'root';   //because we are using xampp
+$password = '';
+$dbname = 'iminchinuch';
+$conn = mysqli_connect($servername, $username, $password, $dbname)
+?>;
