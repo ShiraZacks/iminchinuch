4b1a8ffc328d902db89bedb558f2258141d5c37d
ShiraZacks <shiramzacks@gmail.com>
2023-03-30 13:36:40 -0400

    finished month page sql code

--- month.php
+++ month.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link rel="stylesheet" type='text/css' href="style2.css?<?= time() ?>">
     <link rel="stylesheet" type='text/css' href="style.css?<?= time() ?>">
     <?php include 'menu.php'; ?>
 </head>
