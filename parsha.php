1f2e6bdddfca234a39fa3532f60fbf4616d84771
Shira <shiramzacks@gmail.com>
2023-05-09 15:45:40 -0400

    working on parsha as variable

--- parsha.php
+++ parsha.php
@@ -12,134 +12,151 @@
 </head>
 
 <body>
-<h3>This page is under construction. Sorry!!</h3>
+    <h3>This page is under construction. Sorry!!</h3>
 
-    <!-- all the parshiyos go here, all with div class of their parsha so i can make things work in js -->
     <?php
-include 'dbconnect.php';
-include 'hebrewDate.php';
-switch ($parshaNum) {
-    case 1:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=1') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=1') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=1') or die();
-        break;
+    require 'vendor/autoload.php';
+    include 'dbconnect.php';
 
-    case 2:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=2') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=2') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=2') or die();
-        break;
+    //gets this week's parsha in variable named $currentParsha
+    use Zman\Zman;
 
-    case 3:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=3') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=3') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=3') or die();
-        break;
-
-    case 4:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=4') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=4') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=4') or die();
-        break;
-
-    case 5:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=5') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=5') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=5') or die();
-        break;
-
-    case 6:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=6') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=6') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=6') or die();
-        break;
-
-    case 7:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=7') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=7') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=7') or die();
-        break;
-
-    case 8:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=8') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=8') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=8') or die();
-        break;
-
-    case 9:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=9') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=9') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=9') or die(); 
-        break;
-
-    case 10:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=10') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=10') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=10') or die();
-        break;
-
-    case 11:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=11') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=11') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=11') or die();
-        break;
-
-    case 12:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=12') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=12') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=12') or die();
-        break;
-
-    case 13:
-        $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=13') or die();
-        $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=13') or die();
-        $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=13') or die();
-        break;
-
-    default:
-        print "This is still under construction! Sorry!";
-        break;
-}
-$result1 = mysqli_fetch_array($parshaHebrew);
-$result2 = mysqli_fetch_array($parshaAbout);
-$result3 = mysqli_fetch_array($parshaDeeper);
+    $today = new DateTime();
+    $currentParsha = Zman::parse($today)->parshaHebrew;
 
 
-?>
 
 
-<body>
-    <div class="header2">
-        <h1 class="bigHeader2">
-            <?php
-            print $result1['parshaHebrew'];
-            ?>
-        </h1>
-    </div>
-    <div class="firstMain2">
-        <h5 class="firstBold2">
-        <?php
-            print $result2['parshaAbout'];
-            ?>
-        </h5>
-        <p class="firstSmall2">
-        <?php
-            print $result3['parshaDeeper'];
-            ?>
-        </p>
-    </div>
-    </div>
-    </div>
+    switch ($currentParsha) {
+        case $currentParsha:
+            echo "This week's Parsha is ", $currentParsha;
+
+            $stmt = $conn->prepare('SELECT parshaHebrew FROM `parsha` WHERE parshaHebrew=?');
+            $stmt->bind_param("s", $currentParsha);
+            $stmt->execute();
+            $parshaHebrew = $stmt->get_result();
+           /// $parshaHebrew = $result->fetch_assoc();
+        
 
 
-    <!-- bottom footer -->
-    <footer>
-        <p class="footer">Website created by Shira Zacks. Please <a href="contact.php">contact me</a> if there are any mistakes; only Hashem
-            is perfect!</p>
-    </footer>
+
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=1') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=1') or die();
+            break;
+
+        case 2:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=2') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=2') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=2') or die();
+            break;
+
+        case 3:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=3') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=3') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=3') or die();
+            break;
+
+        case 4:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=4') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=4') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=4') or die();
+            break;
+
+        case 5:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=5') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=5') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=5') or die();
+            break;
+
+        case 6:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=6') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=6') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=6') or die();
+            break;
+
+        case 7:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=7') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=7') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=7') or die();
+            break;
+
+        case 8:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=8') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=8') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=8') or die();
+            break;
+
+        case 9:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=9') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=9') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=9') or die();
+            break;
+
+        case 10:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=10') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=10') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=10') or die();
+            break;
+
+        case 11:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=11') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=11') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=11') or die();
+            break;
+
+        case 12:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=12') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=12') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=12') or die();
+            break;
+
+        case 13:
+            $parshaHebrew = mysqli_query($conn, 'SELECT parshaHebrew FROM `parsha` WHERE id=13') or die();
+            $parshaAbout = mysqli_query($conn, 'SELECT parshaAbout FROM `parsha` WHERE id=13') or die();
+            $parshaDeeper = mysqli_query($conn, 'SELECT parshaDeeper FROM `parsha` WHERE id=13') or die();
+            break;
+
+        default:
+            print "This is still under construction! Sorry!";
+            break;
+    }
+    $result1 = mysqli_fetch_array($parshaHebrew);
+    $result2 = mysqli_fetch_array($parshaAbout);
+    $result3 = mysqli_fetch_array($parshaDeeper);
+    ?>
+
+    <body>
+        <div class="header2">
+            <h1 class="bigHeader2">
+                <?php
+                print "פרשת ";
+                print $result1['parshaHebrew'];
+                ?>
+            </h1>
+        </div>
+        <div class="firstMain2">
+            <h5 class="firstBold2">
+                <?php
+                print $result2['parshaAbout'];
+                ?>
+            </h5>
+            <p class="firstSmall2">
+                <?php
+                print $result3['parshaDeeper'];
+                ?>
+            </p>
+        </div>
+        </div>
+        </div>
 
 
-</body>
+        <!-- bottom footer -->
+        <footer>
+            <p class="footer">Website created by Shira Zacks. Please <a href="contact.php">contact me</a> if there are any mistakes; only Hashem
+                is perfect!</p>
+        </footer>
+
+
+    </body>
 
 </html>
