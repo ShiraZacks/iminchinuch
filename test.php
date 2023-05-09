cc40cdb87eb0f6d8a1268efb1242ea2106045daf
Shira <shiramzacks@gmail.com>
2023-05-09 15:59:20 -0400

    testing stuff

--- test.php
+++ test.php
@@ -3,6 +3,6 @@ require 'vendor/autoload.php';
 
 use Zman\Zman;
 $today = new DateTime();
-$currentParsha = Zman::parse($today)->parsha;
+$currentParsha = Zman::parse($today)->parshaHebrew;
 
 echo $currentParsha;
