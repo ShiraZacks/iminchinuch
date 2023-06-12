a25c0fe38d148afea73a8f677996ac072ded6dbe
Shira <shiramzacks@gmail.com>
2023-06-12 18:20:56 -0400

    testing out stack overflow answers

--- nextYontif.php
+++ nextYontif.php
@@ -15,68 +15,56 @@ if ($currentYontif == true) {
     print 'nothing going on today!';
 };
 
-/*
-    $dates = array
-    (
-        '0' => "2013-02-18 05:14:54",
-        '1' => "2013-02-12 01:44:03",
-        '2' => "2013-02-05 16:25:07",
-        '3' => "2013-01-29 02:00:15",
-        '4' => "2013-01-27 18:33:45"
-    );
 
-    function closest($dates, $findate)
-    {
-        $newDates = array();
 
-        foreach($dates as $date)
-        {
-            $newDates[] = strtotime($date);
-        }
 
-        echo "<pre>";
-        print_r($newDates);
-        echo "</pre>";
+function nextYontif($dates, $findate)
+{
+    $newDates = array();
 
-        sort($newDates);
-        foreach ($newDates as $a)
-        {
-            if ($a >= strtotime($findate))
-                return $a;
-        }
-        return end($newDates);
+    foreach ($dates as $date) {
+        $newDates[] = strtotime($date);
     }
 
-    $values = closest($dates, date('2013-02-04 14:11:16'));
-    echo date('Y-m-d h:i:s', $values);
+
+    sort($newDates);
+    foreach ($newDates as $a) {
+        if ($a >= strtotime($findate))
+            return $a;
+    }
+    return end($newDates);
+}
+/*
+$values = nextYontif($dates, date($today));
+
+echo date('Y-m-d', $values);
+
+echo "<pre>";
+print_r(array_keys($dates, $values));
+echo "</pre>";
+
+
 */
 
-/*
+$date = $today;
+$allDates = array( //have all yomim tovim like this, with hebrew name, english name, and hebrew date
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('סוכות', 'Sukkos', 'טו תשרי'),
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('שבואות', 'Shavuos', 'ו סיון'),
+    array('שבואות', 'Shavuos', 'ו סיון')
 
-$date = '02/21/2019';
-$allDates = array(
-    '2019-02-17',
-    '02/09/2019',
-    '02/23/2019',
-    '02/18/2019',
-    '02/25/2019',
-    '03/04/2019',
-    '03/11/2019',
-    '03/18/2019',
-    '03/25/2019',
-    '04/01/2019',
-    '04/08/2019',
 );
-function date_sort($a, $b)
-{
-    return strtotime($a) - strtotime($b);
+print_r($allDates[1][0]); //prints סוכות
+
+
+
+
+/*
+foreach ($results['data'] as $result) {
+    echo $results['data'][1]['type'];
 }
-usort($allDates, "date_sort");
-foreach ($allDates as $count => $dateSingle) {
-    if (strtotime($date) < strtotime($dateSingle)) {
-        $nextDate = date('m-d', strtotime($dateSingle));
-        break;
-    }
-}
-echo $nextDate;
 */
