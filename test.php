61d63ec7d202c0e53c2b8dce68fe5769adb22faa
Shira <shiramzacks@gmail.com>
2023-07-09 12:33:42 -0400

    all holidays and fast days typed out

--- test.php
+++ test.php
@@ -42,29 +42,64 @@ $tuBishvatString = Zman::dayOfTuBishvat($year)->toFormattedDateString();
 $tuBishvatDate = strtotime($tuBishvatString);
 $tuBishvat = date('m/d/Y', $tuBishvatDate);
 echo "The date of the upcoming Tu Bishvat is $tuBishvat   ";
+
+$purimString = Zman::dayOfPurim($year)->toFormattedDateString();
+$purimDate = strtotime($purimString);
+$purim = date('m/d/Y', $purimDate);
+echo "The date of the upcoming Purim is $purim   ";
+
+$shushanPurimString = Zman::dayOfShushanPurim($year)->toFormattedDateString();
+$shushanPurimDate = strtotime($shushanPurimString);
+$shushanPurim = date('m/d/Y', $shushanPurimDate);
+echo "The date of the upcoming Shushan Purim is $shushanPurim   ";
 /*
-$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
-$shavuosDate = strtotime($shavuosString);
-$shavuos = date('m/d/Y', $shavuosDate);
-echo "The date of the upcoming Shavuos is $shavuos   ";
-/*
-$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
-$shavuosDate = strtotime($shavuosString);
-$shavuos = date('m/d/Y', $shavuosDate);
-echo "The date of the upcoming Shavuos is $shavuos   ";
-/*
-$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
-$shavuosDate = strtotime($shavuosString);
-$shavuos = date('m/d/Y', $shavuosDate);
-echo "The date of the upcoming Shavuos is $shavuos   ";
-/*
-$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
-$shavuosDate = strtotime($shavuosString);
-$shavuos = date('m/d/Y', $shavuosDate);
-echo "The date of the upcoming Shavuos is $shavuos   ";
-/*
-$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
-$shavuosDate = strtotime($shavuosString);
-$shavuos = date('m/d/Y', $shavuosDate);
-echo "The date of the upcoming Shavuos is $shavuos   ";
+$purimKattanString = Zman::dayOfPurimKattan($year)->toFormattedDateString(); //make purim kattan work if not leap year
+$purimKattanDate = strtotime($purimKattanString);
+$purimKattan = date('m/d/Y', $purimKattanDate);
+if($month === 6 && !isJewishLeapYear($year)){
+    echo "This is not a leap year, no Purim Kattan!";
+}else{
+    echo "The date of the upcoming Purim Kattan is $purimKattan   ";
+}
 */
+$pesachString = Zman::firstDayOfPesach($year)->toFormattedDateString();
+$pesachDate = strtotime($pesachString);
+$pesach = date('m/d/Y', $pesachDate);
+echo "The date of the upcoming Pesach is $pesach   ";
+
+$pesachSheniString = Zman::dayOfPesachSheni($year)->toFormattedDateString();
+$pesachSheniDate = strtotime($pesachSheniString);
+$pesachSheni = date('m/d/Y', $pesachSheniDate);
+echo "The date of the upcoming Pesach Sheni is $pesachSheni   ";
+
+$shavuosString = Zman::firstDayOfShavuos($year)->toFormattedDateString();
+$shavuosDate = strtotime($shavuosString);
+$shavuos = date('m/d/Y', $shavuosDate);
+echo "The date of the upcoming Shavuos is $shavuos   ";
+
+$yomKippurString = Zman::dayOfYomKippur($year)->toFormattedDateString();
+$yomKippurDate = strtotime($yomKippurString);
+$yomKippur = date('m/d/Y', $yomKippurDate);
+echo "The date of the upcoming Yom Kippur is $yomKippur   ";
+
+$tzomGedaliahString = Zman::dayOfTzomGedaliah($year)->toFormattedDateString();
+$tzomGedaliahDate = strtotime($tzomGedaliahString);
+$tzomGedaliah = date('m/d/Y', $tzomGedaliahDate);
+echo "The date of the upcoming Tzom Gedaliah is $tzomGedaliah   ";
+
+$taanisEstherString = Zman::dayOfTaanisEsther($year)->toFormattedDateString();
+$taanisEstherDate = strtotime($taanisEstherString);
+$taanisEsther = date('m/d/Y', $taanisEstherDate);
+echo "The date of the upcoming Taanis Esther is $taanisEsther   ";
+
+$shivaAsarBitamuzString = Zman::dayOfShivaAsarBitamuz($year)->toFormattedDateString();
+$shivaAsarBitamuzDate = strtotime($shivaAsarBitamuzString);
+$shivaAsarBitamuz = date('m/d/Y', $shivaAsarBitamuzDate);
+echo "The date of the upcoming Shiva Asar Bitamuz is $shivaAsarBitamuz   ";
+
+$tishaBavString = Zman::dayOfTishaBav($year)->toFormattedDateString();
+$tishaBavDate = strtotime($tishaBavString);
+$tishaBav = date('m/d/Y', $tishaBavDate);
+echo "The date of the upcoming Tisha Bav is $tishaBav   ";
+
+//18 different yontifs
