20f759b5890dc0a75cd31a97086f8bef5e8a972b
Shira <shiramzacks@gmail.com>
2023-08-17 17:50:53 -0400

    checking for js solutions

--- yontif.php
+++ yontif.php
@@ -29,27 +29,40 @@ use Zman\Zman;
                 <div class="header2">
                     <h1 class="bigHeader2">CHOOSE A YONTIF</h1>
                 </div>
-                <form action="" method="post">
+
+                <script>
+                    // when i click the button it should set the variable $currentYontif to the value
+                    $(document).ready(function() {
+                        //when the <button> button is clicked
+                        $('input').click(function() {
+                            //store the user's entry in a variable
+                            var currentYontif = document.getElementById('yontifButton').value;
+                        });
+
+                    });
+                </script>
+
+                <!-- <form action="<?php //yontif(); ?>" method="post"> -->
                     <div class="yontifOptions">
-                        <input type="submit" href="#header1" <?php $currentYontif = "ראש השנה" ?> class="buttonStyles yontifStyles" value="Rosh Hashana"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "צום גדליה" ?> class="buttonStyles yontifStyles" value="Tzom Gedalia"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "יום כפור" ?> class="buttonStyles yontifStyles" value="Yom Kippur"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "סוכות" ?> class="buttonStyles yontifStyles" value="Sukkos"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "שמיני עצרת" ?> class="buttonStyles yontifStyles" value="Shmini Atzeres"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "שמחת תורה" ?> class="buttonStyles yontifStyles" value="Simchas Torah"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "חנוכה" ?> class="buttonStyles yontifStyles" value="Chanuka"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "טו בשבת" ?> class="buttonStyles yontifStyles" value="Tu Bishvat"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "תענית אסתר" ?> class="buttonStyles yontifStyles" value="Taanis Esther"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "פורים" ?> class="buttonStyles yontifStyles" value="Purim"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "שושן פורים" ?> class="buttonStyles yontifStyles" value="Shushan Purim"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "פסח" ?> class="buttonStyles yontifStyles" value="Pesach"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "פסח שני" ?> class="buttonStyles yontifStyles" value="Pesach Sheini"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "שבעות" ?> class="buttonStyles yontifStyles" value="Shavuos"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "שבע עשר בתמוז" ?> class="buttonStyles yontifStyles" value="Shiva Asar Bitamuz"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "תשע באב" ?> class="buttonStyles yontifStyles" value="Tisha Bav"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "טו באב" ?> class="buttonStyles yontifStyles" value="Tu Bav"></input>
-                        <input type="submit" href="#header1" <?php $currentYontif = "עשרת ימי תשובה" ?> class="buttonStyles yontifStyles" value="Aseres Yemei Teshuva"></input>
-                </form>
+                        <input type="button" href="#header1" <?php $currentYontif = "ראש השנה" ?> class="buttonStyles yontifStyles" id="<?php $currentYontif ?>" value="Rosh Hashana"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "צום גדליה" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Tzom Gedalia"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "יום כפור" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Yom Kippur"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "סוכות" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Sukkos"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "שמיני עצרת" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Shmini Atzeres"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "שמחת תורה" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Simchas Torah"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "חנוכה" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Chanuka"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "טו בשבת" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Tu Bishvat"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "תענית אסתר" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Taanis Esther"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "פורים" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Purim"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "שושן פורים" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Shushan Purim"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "פסח" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Pesach"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "פסח שני" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Pesach Sheini"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "שבעות" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Shavuos"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "שבע עשר בתמוז" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Shiva Asar Bitamuz"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "תשע באב" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Tisha Bav"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "טו באב" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Tu Bav"></input>
+                        <input type="button" href="#header1" <?php $currentYontif = "עשרת ימי תשובה" ?> class="buttonStyles yontifStyles" id="yontifButton" value="Aseres Yemei Teshuva"></input>
+                <!-- </form> -->
                 <h1><?php echo $currentYontif; ?></h1>
             </div>
             <!-- get each one to call that sql section, put it in the below sections. -->
