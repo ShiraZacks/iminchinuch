f57c91c08760126185bf074e39d040491495e18c
Shira Zacks <shiramzacks@gmail.com>
2023-10-09 13:27:21 -0400

    minor changes

--- davening.php
+++ davening.php
@@ -3,23 +3,35 @@ require 'vendor/autoload.php';
 
 use Zman\Zman;
 $today = new DateTime();
-
+$hebrewYear = Zman::now()->jewishYear;
+$elul = Zman::firstDayOfSfardiSlichos($hebrewYear)->toFormattedDateString();
+$roshHashana = Zman::createFromJewishDate($hebrewYear, 1, 1);
+$shminiAtzeres = Zman::dayOfShminiAtzeres($hebrewYear)->toFormattedDateString();
 $mussaf = Zman::parse($today)->hasMussaf();
 $hallel = Zman::parse($today)->hasHallel();
 $slichos = Zman::parse($today)->hasSlichos();
 
-if (($mussaf == true)&&($hallel == true)&&($slichos == true)) {
-    print "Today you should be davening Mussaf and Hallel! We also say S'lichos.";
-}elseif(($mussaf == true)&&($hallel == true)){
-    print "Today you should be davening Mussaf and Hallel!";
-}elseif($hallel == true){
-    print "Today is a Hallel day, no Mussaf though.";
-}elseif($mussaf==true){
-    print "Today is a Mussaf day, no Hallel though.";
-}elseif($slichos == true){
-    print "We say S'lichos today.";
-}else{
-    print "No additions to the regular t'fillos";
-};
+print $roshHashana;
+print $elul;
+print $shminiAtzeres;
 
-?>
+if (($mussaf == true) && ($hallel == true) && ($slichos == true)) {
+    print "Today you should be davening Mussaf and Hallel! We also say S'lichos.";
+} elseif (($mussaf == true) && ($hallel == true)) {
+    print "Today you should be davening Mussaf and Hallel!";
+} elseif ($hallel == true) {
+    print "Today is a Hallel day, no Mussaf though.";
+} elseif ($mussaf == true) {
+    print "Today is a Mussaf day, no Hallel though.";
+} elseif ($slichos == true) {
+    print "We say S'lichos today.";
+} else {
+    print "No additions to the regular t'fillos";
+}
+;
+
+if (($today > $roshHashana) && ($today < $shminiAtzeres)) {
+    print "We add L'David today!";
+} else {
+    print "No L'David";
+}
--- parsha.php
+++ parsha.php
@@ -6,12 +6,12 @@
     <link rel="shortcut icon" type="image/jpeg" href="favcon.jpg" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?= time() ?>"></script>
+    <script type="text/javascript" charset="utf-8" src="toggleBars.js?<?=time()?>"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=David+Libre&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="style.css?<?= time() ?>">
-    <?php include 'menu.php'; ?>
+    <link rel="stylesheet" href="style.css?<?=time()?>">
+    <?php include 'menu.php';?>
 </head>
 
 <body>
@@ -19,66 +19,61 @@
         <div class="contentWrap">
 
             <?php
-            require 'vendor/autoload.php';
-            include 'dbconnect.php';
+require 'vendor/autoload.php';
+include 'dbconnect.php';
 
-            //gets this week's parsha in variable named $currentParsha
-            use Zman\Zman;
+//gets this week's parsha in variable named $currentParsha
+use Zman\Zman;
 
-            $today = new DateTime();
-            $currentParsha = Zman::parse($today)->parshaHebrew;
+$today = new DateTime();
+$currentParsha = Zman::parse($today)->parshaHebrew;
 
+switch ($currentParsha) {
+    case $currentParsha: //this uses the variable to call the sql code, super exciting it works!!!
+        $stmt = $conn->prepare('SELECT parshaHebrew, parshaAbout, parshaDeeper FROM `parsha` WHERE parshaHebrew=?');
+        $stmt->bind_param("s", $currentParsha);
+        $stmt->execute();
+        $parshaInfo = $stmt->get_result();
+        break;
+    default:
+        print "This is still under construction! Sorry!";
+        break;
+}
+$result = mysqli_fetch_array($parshaInfo);
 
-
-
-            switch ($currentParsha) {
-                case $currentParsha:  //this uses the variable to call the sql code, super exciting it works!!!
-                    $stmt = $conn->prepare('SELECT parshaHebrew, parshaAbout, parshaDeeper FROM `parsha` WHERE parshaHebrew=?');
-                    $stmt->bind_param("s", $currentParsha);
-                    $stmt->execute();
-                    $parshaInfo = $stmt->get_result();
-
-
-                    break;
-
-                default:
-                    print "This is still under construction! Sorry!";
-                    break;
-            }
-            $result = mysqli_fetch_array($parshaInfo);
-
-            $parshaThisWeekBool = is_null($result);
-            if ($parshaThisWeekBool == true) {
-                print "    <div class='firstTop noParshaText'>
+$parshaThisWeekBool = is_null($result);
+if ($parshaThisWeekBool == true) {
+    print "    <div class='firstTop noParshaText'>
                 <h2 class='firstBold'>There is no Parsha this week because of Yontif!</h2>
                 <h1 class='firstBold'>😊</h1>
             </div>"; //comeback
-            } else { ?>
+} else {?>
                 <div class="header2">
                     <h1 class="bigHeader2">
                         <?php
-                        print "פרשת ";
-                        print $result['parshaHebrew'];
-                        ?>
+print "פרשת ";
+    print $result['parshaHebrew'];
+    ?>
                     </h1>
                 </div>
                 <div class="firstMain2">
                     <h5 class="firstBold2">
                         <?php
-                        print $result['parshaAbout'];
-                        ?>
+print $result['parshaAbout'];
+    ?>
                     </h5>
                     <p class="firstSmall2">
                         <?php
-                        print $result['parshaDeeper'];
-                        ?>
+print $result['parshaDeeper'];
+    ?>
                     </p>
                 </div>
         </div>
     <?php
-            }; ?>
+}
+;?>
     <!-- bottom footer -->
-    <?php include "footer.php"; ?>
+    <?php include "footer.php";?>
 
 
     </div>
