00030ce90dd34cd9b026ea27063f9ad934ee7517
Shira Zacks <shiramzacks@gmail.com>
2023-10-09 23:15:14 -0400

    put contact me last on menu

--- menu.php
+++ menu.php
@@ -10,9 +10,9 @@ echo ' <!-- Navbar  -->
             <a href="parsha.php" class="buttons">This Week\'s Parsha</a>
             <a href="month.php" class="buttons">This Month</a>
             <a href="yontif.php" class="buttons">Next Yom Tov</a>
-            <a href="contact.php" class="buttons">Contact Me</a>
             <a href="quotes.php" class="buttons">Quote of the Day</a>
             <a href="geula.php" class="buttons">Bring the Geula</a>
+            <a href="contact.php" class="buttons">Contact Me</a>
         </div>
 
         <!-- Navbar on small screens -->
@@ -22,10 +22,10 @@ echo ' <!-- Navbar  -->
                 <li><a href="parsha.php" class="button">This Week\'s Parsha</a> </li>
                 <li><a href="month.php" class="button">This Month</a></li>
                 <li><a href="yontif.php" class="button">Next Yom Tov</a></li>
-                <li><a href="contact.php" class="button">Contact Me</a></li>
                 <li><a href="quotes.php" class="button">Quote of the Day</a></li>
-                <li><a href="geula.php" class="button bottom">Bring the Geula</a></li>
-            </ul>
+                <li><a href="geula.php" class="button">Bring the Geula</a></li>
+                <li><a href="contact.php" class="button bottom">Contact Me</a></li>
+                </ul>
         </div>
     </div>
     <div class="under_the_menu"></div>
